62a4a1d6ff5e7886eaaf0db1ea1ce7deffc1064b
nikhithsunil <nikhithsunil@gmail.com>
2023-01-13 11:33:06 +0530

    recreating widget tree limit issue

---
+++ android/app/src/main/kotlin/com/example/widget_tree_limit/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.widget_tree_limit
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/providers/notifier.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class Provider1 with ChangeNotifier {
+  int _counter = 0;
+  int get counter => _counter;
+  set counter(int value) {
+    _counter = value;
+    notifyListeners();
+  }
+}
